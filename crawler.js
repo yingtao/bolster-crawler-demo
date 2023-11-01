bb9f0aee1bd415d75357f1958fff347da43e1a67
toniguan <toniguan@gmail.com>
2023-11-01 10:53:46 -0700

    fix Page crashed issue by tweak the pupeteer launch parameter

--- crawler.js
+++ crawler.js
@@ -18,7 +18,15 @@ const crawl = async (entry, options = {}) => {
   }
 
   const entryUrl = url.parse(target.url);
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ],
+  });
   const page = await browser.newPage();
   debug.crawl('Puppeteer started');
 
