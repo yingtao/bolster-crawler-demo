25b193cb191c6c7fbec162da28a1521c81fa66b2
toniguan <toniguan@gmail.com>
2023-11-01 11:31:41 -0700

    add a skeleton api server to accept crawl request

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function startCrawl(url) {
+  const { stdout, stderr } = await exec(`./crawl -u ${url}`);
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// create
+app.post('/v1/startcrawl', async (req, res) => {
+  console.log(req.body);
+  const { startUrl } = req.body;
+  await startCrawl(startUrl);
+  res.status(200);
+});
+
+app.get('/', async (req, res) => {
+  console.log('It works!');
+});
+
+app.listen(PORT, () => console.log(`app is running on port: ${PORT}`));
